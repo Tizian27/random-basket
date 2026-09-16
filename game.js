9c7b6c390f96a9f044922f6b2411b6595a6278d0
Proyolo-ks1 <95938542+Proyolo-ks1@users.noreply.github.com>
2026-09-16 11:34:43 +0200

    added player and background

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("game");
+
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
 const scoreElement = document.getElementById("score");
@@ -10,35 +13,59 @@ let score = 0;
 let lives = 3;
 
 const keys = {};
+const wind = 0
+const gravity = -1
 
 // --------------------------------
 // Spieler
 // --------------------------------
 
-const player = {
+function drawCircleF(x, y, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(
+        x,
+        y,
+        radius,
+        0,
+        Math.PI * 2
+    );
+    ctx.fill();
+}
+
+// --------------------------------
+// Spieler
+// --------------------------------
+
+const player1 = {
     x: 100,
     y: 100,
-
     width: 40,
     height: 40,
-
     speed: 5,
-
     color: "dodgerblue"
 };
+
+const player2 = {
+    x: 300,
+    y: 100,
+    width: 40,
+    height: 40,
+    speed: 5,
+    color: "blue"
+};
+
+let player = [player1, player2]
 
 const ball = {
     x: 100,
     y: 100,
-
-    width: 10,
-    height: 10,
-
+    radius: 10,
     speed: 5,
-
     color: "red"
-
 }
+
+let physicsObjects = [player1, player2, ball]
 
 // --------------------------------
 // Eingabe
@@ -62,8 +89,8 @@ function startGame() {
     score = 0;
     lives = 3;
 
-    player.x = 100;
-    player.y = 100;
+    player1.x = 100;
+    player1.y = 100;
 
     gameRunning = true;
 
@@ -79,49 +106,59 @@ function update() {
         return;
     }
 
-    // Bewegung
+    // CONTROLS
     if (keys["w"]) {
-        player.y -= player.speed;
+        player1.y -= player1.speed;
     }
 
     if (keys["s"]) {
-        player.y += player.speed;
+        player1.y += player1.speed;
     }
 
     if (keys["a"]) {
-        player.x -= player.speed;
+        player1.x -= player1.speed;
     }
 
     if (keys["d"]) {
-        player.x += player.speed;
+        player1.x += player1.speed;
     }
 
     if (keys["arrowdown"]) {
-        ball.y += ball.speed
+        player2.y += player2.speed
     }
 
     if (keys["arrowup"]){
-        ball.y -= ball.speed
+        player2.y -= player2.speed
     }
 
     if(keys["arrowleft"]){
-        ball.x -= ball.speed
+        player2.x -= player2.speed
     }
 
     if(keys["arrowright"]){
-        ball.x += ball.speed
+        player2.x += player2.speed
     }
 
     // Spielfeldbegrenzung
-    player.x = Math.max(
-        0,
-        Math.min(canvas.width - player.width, player.x)
-    );
+    player.forEach(player => {
+        player.x = Math.max(
+            0,
+            Math.min(canvas.width - player.width, player.x)
+        );
+        player.y = Math.max(
+            0,
+            Math.min(canvas.height - player.height, player.y)
+        );
+    });
 
-    player.y = Math.max(
-        0,
-        Math.min(canvas.height - player.height, player.y)
-    );
+    // PHYSICS
+    physicsObjects.forEach(object => {
+        object.velY += gravity;
+        object.velX += wind; //maybe wind?
+
+        object.posY += object.velY;
+        object.posX += object.velX;
+    });
 }
 
 // --------------------------------
@@ -131,30 +168,26 @@ function update() {
 function draw() {
 
     // Hintergrund
-    ctx.fillStyle = "#222";
+    ctx.fillStyle = "#6bbfd9";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    grass_height = 100
+    ctx.fillStyle = "#51c468";
+    ctx.fillRect(0, canvas.height - grass_height, canvas.width, grass_height);
 
     // Spieler
+    player.forEach(player => {
     ctx.fillStyle = player.color;
-
-    ctx.fillRect(
-        player.x,
-        player.y,
-        player.width,
-        player.height
-    );
+        ctx.fillRect(
+            player.x,
+            player.y,
+            player.width,
+            player.height
+        );
+    })
 
     // Spieler
-    ctx.fillStyle = ball.color;
-
-    ctx.fillRect(
-        ball.x,
-        ball.y,
-        ball.width,
-        ball.height
-    );
-
-
+    drawCircleF(ball.x, ball.y, ball.radius, ball.color)
 
     // Start-Hinweis
     if (!gameRunning) {
